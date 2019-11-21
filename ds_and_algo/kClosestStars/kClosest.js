a99753dc10defa59f5c287cf10d1f1cbd8cd81d6
William Miao <404.error.not.found@gmail.com>
2019-11-21 04:23:52 -0800

    kClosest uses heap

--- ds_and_algo/kClosestStars/kClosest.js
+++ ds_and_algo/kClosestStars/kClosest.js
@@ -1,8 +1,8 @@
-const MaxHeap = require('../heap/MaxHeap');
+const Heap = require('../heap/Heap').Heap;
 
 module.exports = function kClosest(stars, k, n) {
     const arr = new Array(k).fill(Number.MAX_SAFE_INTEGER);
-    const heap = new MaxHeap(arr);
+    const heap = new Heap(arr, (a, b) => a.value < b.value);
     for(let i = 0; i < n; i++) {
         heap.insert(calculateDistance(stars.next()));
         heap.pop();
